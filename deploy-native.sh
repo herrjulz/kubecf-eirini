84436f4e1247c93ae818cd7fd0f782a0c2a93555
Julian Skupnjak <skupnjak@de.ibm.com>
2020-03-23 12:27:46 +0100

    WIP: make helm3 work
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- deploy-native.sh
+++ deploy-native.sh
@@ -3,16 +3,16 @@
 set -ex
 
 main(){
-   install-operator
-   sleep 15
-   install-kubecf
+   # install-operator
+   # sleep 15
+   install-kubecf $1
 }
 
 install-operator(){
-  helm install --namespace cf-operator \
-    --name cf-operator \
+  helm3 install --namespace cf-operator \
+    cf-operator \
     --set "global.operator.watchNamespace=kubecf" \
-    https://s3.amazonaws.com/cf-operators/release/helm-charts/cf-operator-v2.0.0-0.g0142d1e9.tgz
+   https://s3.amazonaws.com/cf-operators/release/helm-charts/cf-operator-3.3.0%2B0.gf32b521e.tgz
 }
 
 install-kubecf(){
@@ -21,12 +21,11 @@ install-kubecf(){
   bits_tls_key="$(kubectl get secret "$cluster_secret" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
   bits_tls_crt="$(kubectl get secret "$cluster_secret" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
 
-  helm install ./helm \
-   --name kubecf \
+  helm3 install kubecf ./helm \
    --namespace kubecf \
-   --values ./eirini-native-values.yml \
+   --values $1 \
    --set "bits.secrets.BITS_TLS_KEY=${bits_tls_key}" \
    --set "bits.secrets.BITS_TLS_CRT=${bits_tls_crt}"
 }
 
-main
+main $1
